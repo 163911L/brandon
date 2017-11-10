65ab913ecdb7bbb2ef04c485284325745012bce3
163911L BRANDON CHOW <163911L@sit.nyp.edu.sg>
2017-11-10 10:17:55 +0800

    my first

--- MathAdd.java
+++ MathAdd.java
@@ -4,5 +4,7 @@ public class MathAdd {
 	
 	static public int add(int a, int b) {   
         return a + b; 
+systemout.println("");
 	}
+
 }
---
+++ MathSub.java
@@ -0,0 +1,9 @@
+public class MathSub {
+	
+	public MathSub {}
+	
+	static public int add(int a, int b) {   
+        return a - b; 
+system.put.println("");
+	}
+}
